44bd4fb449a80e2c384d4fc3d90e900c54a00b9c
Diana Fernández <dianaluciafv@gmail.com>
2025-10-16 00:20:50 -0700

    [ADDED][DIANA] UI Functions
    Added the functions for printing the inventory menu and the different cup type
    
    Co-Authored-By: pg29diana <235600740+pg29diana@users.noreply.github.com>

--- A3/UIManager.cs
+++ A3/UIManager.cs
@@ -1,8 +1,96 @@
 ﻿using System;
+using System.Xml.Serialization;
 
 namespace A3
 {
     public class UIManager
     {
+        public void PrintMenu()
+        {
+            Console.WriteLine("""
+            ===================================================================
+                        Select an ingredient to add to your cup:
+
+               1. Rum      2. Vodka     3. Tequila     4. Gin      5. Whiskey
+               6. Lemon    7. Sugar     8. Ice         9. Soda     10. Mint
+            ===================================================================
+
+
+            """);
+        }
+
+        public void PrintCup(int level)
+        {
+            switch (level)
+            {
+                case 0:
+                   Console.WriteLine("""
+                    \           /
+                     \         /
+                      \       /
+                       \     /
+                        \___/
+                    """);
+                    break;
+
+                case 1:
+                    Console.WriteLine("""
+                     \           /
+                      \         /
+                       \       /
+                        \-----/
+                         \___/ 
+                    """);
+                    break;
+                case 2:
+                    Console.WriteLine("""
+                    \           /
+                     \         /
+                      \-------/
+                       \     /
+                        \___/ 
+                    """);
+                    break;
+                case 3:
+                    Console.WriteLine("""
+                    \           /
+                     \---------/
+                      \       /
+                       \     /
+                        \___/ 
+                    """);
+                    break;
+                default:
+                    Console.WriteLine("Invalid cup level.");
+                    break;
+            }
+        }
+
+        //public void PrintAllCups()
+        //{
+        //    for (int level = 0; level <= 3; level++)
+        //    {
+        //        Console.WriteLine($"=== Cup level {level} ===");
+        //        PrintCup(level);
+        //    }
+        //}
+
+        public void PrintRecipe(List<EIngredient> ingredients) // TODO: agregar nivel de felicidad del cliente
+        {
+            //Console.WriteLine($"""
+            // ________________________________
+            //|                                |  
+            //|            Feliz               | 
+            //|                                | 
+            //|                                | 
+            //|     1.  {ingredients[0]}\t| 
+            //|                                | 
+            //|     2.  {ingredients[1]}\t| 
+            //|                                | 
+            //|     3.  {ingredients[2]}\t| 
+            //|                                | 
+            //---------------------------------
+            //""");
+        }
     }
 }
