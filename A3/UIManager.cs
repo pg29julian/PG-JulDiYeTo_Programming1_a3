18449e1d8ca872607472989c097cbde78483eeea
Diana Fernández <dianaluciafv@gmail.com>
2025-10-21 12:38:37 -0700

    [MODIFIED][DIANA] PrintRecipe Method
    Changing the layout for printing the recepies so now it can print 3 at a time with the 3 ingredients and mood each

--- A3/UIManager.cs
+++ A3/UIManager.cs
@@ -5,6 +5,9 @@ namespace A3
 {
     public class UIManager
     {
+        /// <summary>
+        /// Helper method that prints the ingredient selection menu
+        /// </summary>
         public void PrintMenu()
         {
             Console.WriteLine("""
@@ -19,11 +22,14 @@ namespace A3
             """);
         }
 
+        /// <summary>
+        /// Helper method that prints the cup depending on its current level to indicate how full it is
+        /// </summary>
         public void PrintCup(int level)
         {
             switch (level)
             {
-                case 0:
+                case 0: /// If the level is 0, the cup is empty
                    Console.WriteLine("""
                     \           /
                      \         /
@@ -33,7 +39,7 @@ namespace A3
                     """);
                     break;
 
-                case 1:
+                case 1: /// If the level is 1, the cup has just one ingredient
                     Console.WriteLine("""
                      \           /
                       \         /
@@ -42,7 +48,7 @@ namespace A3
                          \___/ 
                     """);
                     break;
-                case 2:
+                case 2: /// If the level is 2, the cup has two ingredients
                     Console.WriteLine("""
                     \           /
                      \         /
@@ -51,7 +57,7 @@ namespace A3
                         \___/ 
                     """);
                     break;
-                case 3:
+                case 3: /// If the level is 3, the cup is full
                     Console.WriteLine("""
                     \           /
                      \---------/
@@ -60,37 +66,55 @@ namespace A3
                         \___/ 
                     """);
                     break;
-                default:
+                default: /// If the's any other value, it's invalid
                     Console.WriteLine("Invalid cup level.");
                     break;
             }
         }
 
-        //public void PrintAllCups()
-        //{
-        //    for (int level = 0; level <= 3; level++)
-        //    {
-        //        Console.WriteLine($"=== Cup level {level} ===");
-        //        PrintCup(level);
-        //    }
-        //}
-
-        public void PrintRecipe(List<EIngredient> ingredients) // TODO: agregar nivel de felicidad del cliente
+        /// <summary>
+        /// Helper method that prints the recipe cards of the three customers
+        /// </summary>
+        /// The method receives three lists of ingredients and a list of mood levels, one for each customer
+        public void PrintRecipe(List<EIngredient> ingredients1, List<EIngredient> ingredients2, List<EIngredient> ingredients3,
+            List<int> moods)
         {
-            //Console.WriteLine($"""
-            // ________________________________
-            //|                                |  
-            //|            Feliz               | 
-            //|                                | 
-            //|                                | 
-            //|     1.  {ingredients[0]}\t| 
-            //|                                | 
-            //|     2.  {ingredients[1]}\t| 
-            //|                                | 
-            //|     3.  {ingredients[2]}\t| 
-            //|                                | 
-            //---------------------------------
-            //""");
+            Console.WriteLine($"""
+             ________________________________        ________________________________         ________________________________
+            |                                |      |                                |       |                                |
+            |{Center(PrintEmotion(moods[0]),32)}|      |{Center(PrintEmotion(moods[1]),32)}|       |{Center(PrintEmotion(moods[2]),32)}|
+            |                                |      |                                |       |                                |
+            |                                |      |                                |       |                                |
+            |        1.  {ingredients1[0],-20}|      |        1.  {ingredients2[0],-20}|       |        1.  {ingredients3[0],-20}|
+            |                                |      |                                |       |                                |
+            |        2.  {ingredients1[1],-20}|      |        2.  {ingredients2[1],-20}|       |        1.  {ingredients3[1],-20}|
+            |                                |      |                                |       |                                |
+            |        3.  {ingredients1[2],-20}|      |        3.  {ingredients2[2],-20}|       |        3.  {ingredients3[2],-20}|
+            |                                |      |                                |       |                                | 
+            |________________________________|      |________________________________|       |________________________________|
+            """);
+        }
+
+        /// <summary>
+        /// Helper method that returns an emotion string depending on the mood level provided
+        /// </summary>
+        public string PrintEmotion(int moodLevel)
+        {
+            if (moodLevel == 3) return ":)"; /// If the mood is 3, the client is happy
+            else if (moodLevel == 2)return ":|"; /// If the mood is 2, the client is neutral
+            else if (moodLevel == 1) return ">:("; /// If the mood is 1, the client is angry
+            else return "X("; /// If the mood is 0, the client is very angry and is now gone
+        }
+
+        /// <summary>
+        /// Helper method that centers a string within a given width
+        /// </summary>
+        static string Center(string s, int width)
+        {
+            if (string.IsNullOrEmpty(s)) return new string(' ', width);
+            if (s.Length >= width) return s.Substring(0, width);
+            int left = (width - s.Length) / 2;
+            return new string(' ', left) + s + new string(' ', width - left - s.Length);
         }
     }
 }
